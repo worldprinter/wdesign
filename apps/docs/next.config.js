18912a1281f2468f9d269713c334411eafa22941
{authemail@qq.com} <authemail@qq.com>
2023-05-26 14:58:43 +0800

    feat: sync files

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,35 +1,34 @@
 //@ts-check
-process.env.NEXT_ROOT = __dirname
+process.env.NX_NEXT_DIR = __dirname
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next')
 const withNextra = require('nextra')
 
 /**
- * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
+ * @type {import("@nx/next/plugins/with-nx").WithNxOptions}
  **/
 const nextConfig = {
-  nx: {
-    // Set this to true if you would like to use SVGR
-    // See: https://github.com/gregberge/svgr
-    svgr: false,
-  },
-  i18n: {
-    locales: ['en', 'zh'],
-    defaultLocale: 'zh'
-  }
+    nx: {
+        // Set this to true if you would like to use SVGR
+        // See: https://github.com/gregberge/svgr
+        svgr: false,
+    },
+    i18n: {
+        locales: ['en', 'zh'],
+        defaultLocale: 'zh',
+    },
 }
 
 const plugins = [
-  // Add more Next.js plugins to this list if needed.
-  withNx,
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  withNextra({
-    theme: 'nextra-theme-docs',
-    themeConfig: './apps/docs/theme.config.tsx',
-    rootDir: __dirname,
-  }),
+    // Add more Next.js plugins to this list if needed.
+    withNx,
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    withNextra({
+        theme: 'nextra-theme-docs',
+        themeConfig: './apps/docs/theme.config.tsx',
+    }),
 ]
 
 module.exports = composePlugins(...plugins)(nextConfig)

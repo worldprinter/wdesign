2f7df69b132b74afd386b1227ce2b0fbea1032cc
{authemail@qq.com} <authemail@qq.com>
2023-06-12 15:36:45 +0800

    feat: change ssr get next

--- libs/next/src/create-get-initial-props.tsx
+++ libs/next/src/create-get-initial-props.tsx
@@ -1,15 +1,21 @@
-import type { DocumentContext } from 'next/document'
-import NextDocument from 'next/document'
+import type { DocumentContext, DocumentInitialProps } from 'next/document'
 import React from 'react'
 
 import { createStylesServer, ServerStyles } from '@worldprinter/wdesign-ssr'
 import type { EmotionCache } from '@worldprinter/wdesign-styles'
 
-export function createGetInitialProps(cache?: EmotionCache): (ctx: DocumentContext) => any {
+export function createGetInitialProps(
+    /**
+     * Next.js Document.getInitialProps
+     * @see https://nextjs.org/docs/advanced-features/custom-document
+     */
+    NextInitialProps: (ctx: DocumentContext) => Promise<DocumentInitialProps>,
+    cache?: EmotionCache,
+): (ctx: DocumentContext) => any {
     const stylesServer = createStylesServer(cache)
 
     return async function getInitialProps(ctx: DocumentContext) {
-        const initialProps = await NextDocument.getInitialProps(ctx)
+        const initialProps = await NextInitialProps(ctx)
         return {
             ...initialProps,
             styles: (
